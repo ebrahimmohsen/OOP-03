6700d8a2c44ebfd0d6d71369feb55e5d0693e71b
ebrahimmohsen <ebrahimzaki456@gmail.com>
2025-07-14 02:58:06 +0300

    Q2 solved

--- OOP 03/Program.cs
+++ OOP 03/Program.cs
@@ -66,12 +66,77 @@ namespace OOP_03
             }
         }
         #endregion
+
+        #region Q2 Develop a Class to represent the Hiring Date Data
+        class HireDate
+        {
+            private int day, month, year;
+
+            public int Day
+            {
+                get => day;
+                set
+                {
+                    if (value >= 1 && value <= 31)
+                        day = value;
+                    else
+                        throw new ArgumentException("Day must be between 1 and 31.");
+                }
+            }
+
+            public int Month
+            {
+                get => month;
+                set
+                {
+                    if (value >= 1 && value <= 12)
+                        month = value;
+                    else
+                        throw new ArgumentException("Month must be between 1 and 12.");
+                }
+            }
+
+            public int Year
+            {
+                get => year;
+                set
+                {
+                    if (value >= 1900 && value <= DateTime.Now.Year)
+                        year = value;
+                    else
+                        throw new ArgumentException("Invalid year.");
+                }
+            }
+
+            public HireDate(int day, int month, int year)
+            {
+                Day = day;
+                Month = month;
+                Year = year;
+            }
+
+            public override string ToString()
+            {
+                return $"{Day:00}/{Month:00}/{Year}";
+            }
+
+            public DateTime ToDateTime()
+            {
+                return new DateTime(Year, Month, Day);
+            }
+        }
+        #endregion
         static void Main(string[] args)
         {
             #region Q1 Design and implement a Class for the employees in a company:
 
             Employee emp = new Employee(1, "Alice", 'F', SecurityLevel.Developer, 8500);
             Console.WriteLine(emp);
+            #endregion
+
+            #region Q2 Develop a Class to represent the Hiring Date Data
+            HireDate date = new HireDate(15, 5, 2021);
+            Console.WriteLine("Hire Date: " + date);
             #endregion
         }
     }
